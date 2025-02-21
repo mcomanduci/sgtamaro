5d2b448100ab7636fcb4621d3e189ae2c8cf677f
Marcelo Comanduci <mcomanduci@gmail.com>
2025-02-21 09:48:22 -0300

    responsive

--- js.js
+++ js.js
@@ -3,7 +3,10 @@ const foto = document.querySelector("#foto");
 const spider = document.querySelector("#spider");
 const amaru = document.querySelector("#amaru");
 const push = document.querySelector("#push");
+const nestor = document.querySelector("#nestor");
+const coruja = document.querySelector("#coruja");
 const audio = document.querySelector("audio");
+const video = document.querySelector("video");
 
 setTimeout(() => {
   text.style.opacity = 0;
@@ -22,15 +25,16 @@ setTimeout(() => {
 
 setTimeout(() => {
   spider.classList.remove("translate-y-[-1000px]");
+  spider.classList.remove("translate-x-[-1000px]");
 }, 12000);
 
 setTimeout(() => {
   amaru.classList.remove("translate-x-[1000px]");
-  amaru.classList.add("translate-x-[200px]");
+  amaru.classList.add("md:translate-x-[230px]");
 }, 14000);
 
 setTimeout(() => {
-  push.classList.remove("translate-x-[-2000px]");
+  push.classList.remove("translate-x-[2000px]");
   push.classList.add("translate-x-1/2");
   setInterval(() => {
     push.src = "./flexao-down.png";
@@ -39,3 +43,22 @@ setTimeout(() => {
     }, 500);
   }, 1000);
 }, 16000);
+
+setTimeout(() => {
+  nestor.classList.remove("translate-x-[-1000px]");
+}, 18000);
+
+setTimeout(() => {
+  coruja.classList.remove("translate-y-[1000px]");
+  coruja.classList.add("-translate-y-1/2");
+  coruja.classList.add("translate-x-1/2");
+}, 20000);
+
+setTimeout(() => {
+  video.classList.remove("hidden");
+  video.classList.remove("opacity-0");
+}, 24000);
+
+setTimeout(() => {
+  video.classList.add("hidden");
+}, 30500);
